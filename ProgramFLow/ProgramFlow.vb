0724bc8b98cf466676d0c87b528b4c95d9e80518
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-01-27 08:34:42 -0700

    added if, and if, & elseif

--- ProgramFLow/ProgramFlow.vb
+++ ProgramFLow/ProgramFlow.vb
@@ -1,7 +1,37 @@
-﻿Module ProgramFlow
+﻿'Alexis Villagran
+'RCET
+'1/27/2013
+'Program Flow
+
+Option Strict On
+Option Explicit On
+
+Module ProgramFlow
 
     Sub Main()
+        Dim num1 As Integer
+        num1 = 5
 
+        If num1 > 6 Then
+            Console.WriteLine(num1 > 6)
+        End If
+
+        If num1 > 6 Then
+            Console.WriteLine(num1 > 3)
+        End If
+
+        If num1 > 4 Then
+            Console.WriteLine(num1 > 4)
+        End If
+
+        'If anything evaluates to true all other evaluations are skipped
+        If num1 > 6 Then
+            Console.WriteLine(num1 > 6)
+        ElseIf num1 > 3 Then
+            Console.WriteLine(num1 > 3)
+        ElseIf num1 > 4 Then
+            Console.WriteLine(num1 > 4)
+        End If
     End Sub
 
 End Module
