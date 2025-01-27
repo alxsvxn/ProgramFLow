de9449051d3748e04cad30f30e73a824dd54e8b4
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-01-27 08:53:53 -0700

    Commented out some code & added cases

--- ProgramFLow/ProgramFlow.vb
+++ ProgramFLow/ProgramFlow.vb
@@ -10,28 +10,62 @@ Module ProgramFlow
 
     Sub Main()
         Dim num1 As Integer
-        num1 = 5
+        Dim userInput As String
 
-        If num1 > 6 Then
-            Console.WriteLine(num1 > 6)
-        End If
+        'num1 = 5
 
-        If num1 > 6 Then
-            Console.WriteLine(num1 > 3)
-        End If
+        'If num1 > 6 Then
+        '    Console.WriteLine(num1 > 6)
+        'End If
 
-        If num1 > 4 Then
-            Console.WriteLine(num1 > 4)
-        End If
+        'If num1 > 6 Then
+        '    Console.WriteLine(num1 > 3)
+        'End If
 
-        'If anything evaluates to true all other evaluations are skipped
-        If num1 > 6 Then
-            Console.WriteLine(num1 > 6)
-        ElseIf num1 > 3 Then
-            Console.WriteLine(num1 > 3)
-        ElseIf num1 > 4 Then
-            Console.WriteLine(num1 > 4)
-        End If
+        'If num1 > 4 Then
+        '    Console.WriteLine(num1 > 4)
+        'End If
+
+        ''If anything evaluates to true all other evaluations are skipped
+        'If num1 > 6 Then
+        '    Console.WriteLine(num1 > 6)
+        'ElseIf num1 > 3 Then
+        '    Console.WriteLine(num1 > 3)
+        'ElseIf num1 > 4 Then
+        '    Console.WriteLine(num1 > 4)
+        'End If
+
+        'num1 -= 3
+        ''If none of the conditions are true then ELse clause triggers
+        'If num1 > 6 Then
+        '    Console.WriteLine($"{num1} is a bigger number.")
+        'ElseIf num1 > 3 Then
+        '    Console.WriteLine($"Not enough.")
+        'ElseIf num1 > 4 Then
+        '    Console.WriteLine($"Still Not enough.")
+        'Else
+        '    Console.WriteLine($"Not sure what happened")
+        'End If
+
+        Console.WriteLine("Please enter your age:")
+        userInput = Console.ReadLine()
+        num1 = CInt(userInput)
+
+        Select Case num1
+            Case 0 To 3
+
+            Case 4 To 10
+
+            Case 11 To 64
+
+            Case 65 To 99
+
+            Case > 100
+
+            Case Else
+
+
+        End Select
     End Sub
 
 End Module
