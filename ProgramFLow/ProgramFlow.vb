d6ff2b9497ae86ee3d7d685496ae2201a7091a1b
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-01-28 08:54:01 -0700

    Added Do & Do While loops

--- ProgramFLow/ProgramFlow.vb
+++ ProgramFLow/ProgramFlow.vb
@@ -1,6 +1,7 @@
 ﻿Option Strict On
 Option Explicit On
 Option Compare Text
+
 'Alexis Villagran
 'RCET
 '1/27/2013
@@ -11,7 +12,7 @@ Imports System.Linq.Expressions
 Module ProgramFlow
 
     Sub Main()
-        Dim num1 As Integer
+        'Dim num1 As Integer
         Dim userInput As String
 
         'num1 = 5
@@ -49,33 +50,45 @@ Module ProgramFlow
         '    Console.WriteLine($"Not sure what happened")
         'End If
 
+        '    Do
+        '        Console.WriteLine("Please enter your age:")
+        '        userInput = Console.ReadLine()
+
+        '        Try 'Try will run this code first and read the userinput to see if it's a valid integer or "Q" 
+        '            num1 = CInt(userInput)
+        '            Select Case num1
+        '                Case 0 To 3
+        '                    Console.WriteLine("TOO TINY!")
+        '                Case 4 To 10
+        '                    Console.WriteLine("You're still to young to enter.")
+        '                Case 11 To 64
+        '                    Console.WriteLine("Enjoy the ride!")
+        '                Case 65 To 99
+        '                    Console.WriteLine("Please sign this liability release form")
+        '                Case > 100
+        '                    Console.WriteLine("gramps")
+        '                Case Else
+        '                    Console.WriteLine("Invalid")
+        '            End Select
+        '        Catch ex As Exception 'If user entered non-integer, it reiterate input
+        '            Console.WriteLine($"You entered {userInput}.")
+        '        End Try
+
+        '    Loop Until userInput = "Q"
+
+        '    Console.WriteLine("Have a nice day.")
+
+
         Do
-            Console.WriteLine("Please enter your age:")
+            Console.WriteLine("Type sommething & Press Enter")
             userInput = Console.ReadLine()
-
-            Try 'Try will run this code first and read the userinput to see if it's a valid integer or "Q" 
-                num1 = CInt(userInput)
-                Select Case num1
-                    Case 0 To 3
-                        Console.WriteLine("TOO TINY!")
-                    Case 4 To 10
-                        Console.WriteLine("You're still to young to enter.")
-                    Case 11 To 64
-                        Console.WriteLine("Enjoy the ride!")
-                    Case 65 To 99
-                        Console.WriteLine("Please sign this liability release form")
-                    Case > 100
-                        Console.WriteLine("gramps")
-                    Case Else
-                        Console.WriteLine("Invalid")
-                End Select
-            Catch ex As Exception 'If user entered non-integer, it reiterate input
-                Console.WriteLine($"You entered {userInput}.")
-            End Try
-
+            Console.WriteLine($"In the Do Until Loop.")
         Loop Until userInput = "Q"
 
-        Console.WriteLine("Have a nice day.")
+        Do While userInput = "Q"
+            Console.WriteLine("Type sommething & Press Enter")
+            userInput = Console.ReadLine()
+            Console.WriteLine($"In the Do While Loop.")
+        Loop
     End Sub
-
 End Module
